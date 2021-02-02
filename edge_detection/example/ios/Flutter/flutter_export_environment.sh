dafda266a68e720cd70089168cb48136e9fcd3a5
Jacob Moura <jacobaraujo7@gmail.com>
2021-02-02 11:44:52 -0300

    fix publish

--- edge_detection/example/ios/Flutter/flutter_export_environment.sh
+++ edge_detection/example/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\bwolf\fvm\versions\1.22.5"
-export "FLUTTER_APPLICATION_PATH=D:\ADRA\common_packages\iatec_common_packages\edge_detection\example"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/jacobmoura/fvm/versions/master"
+export "FLUTTER_APPLICATION_PATH=/Users/jacobmoura/Projects/IATec/packages/edge_detection/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\bwolf\fvm\versions\1.22.5\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
