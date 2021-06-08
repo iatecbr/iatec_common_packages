f3de6441727ddba6e3999f1a567f47f3aeffa5d2
Daniel Cajazeiro <daniel.cajazeiro@iatec.com>
2021-06-08 14:15:01 -0300

    IATec Localization - v1.0.1

--- iatec_localization/lib/src/external/services/datasources/api_localization.dart
+++ iatec_localization/lib/src/external/services/datasources/api_localization.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:iatec_localization/src/infra/datasources/localization_datasource.dart';
 
+// ignore_for_file: implicit_dynamic_map_literal, implicit_dynamic_parameter, implicit_dynamic_method
 class ApiLocalization implements LocalizationDatasource {
   final Dio dio;
 
@@ -9,20 +10,9 @@ class ApiLocalization implements LocalizationDatasource {
   @override
   Future<Map<String, String>> fetchLocalization(
       String url, String locale) async {
-    final response = await dio.get<Map<String, String>>(
-      url,
-      options: Options(
-        headers: <String, String>{
-          'culture': locale,
-        },
-      ),
-    );
-
-    return (response.data).map<String, String>(
-      (key, value) => MapEntry<String, String>(
-        key.toString(),
-        value.toString(),
-      ),
-    );
+    final response =
+        await dio.get(url, options: Options(headers: {'culture': locale}));
+    return (response.data as Map).map<String, String>((key, value) =>
+        MapEntry<String, String>(key.toString(), value.toString()));
   }
 }
